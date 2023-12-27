129f1d0f8f63f7d395d7cad92a1bc805b2b554a3
Sungbae <9uiopa@gmail.com>
2023-12-27 14:58:22 +0900

    배포 자동화 구성

--- src/main/java/com/uiopa/frspringboot/FrSpringbootApplication.java
+++ src/main/java/com/uiopa/frspringboot/FrSpringbootApplication.java
@@ -2,12 +2,14 @@ package com.uiopa.frspringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 
 @SpringBootApplication
 public class FrSpringbootApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FrSpringbootApplication.class, args);
+		SpringApplication application = new SpringApplication(FrSpringbootApplication.class);
+		application.addListeners(new ApplicationPidFileWriter());
+		application.run(args);
 	}
 }
