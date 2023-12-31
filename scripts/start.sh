1e44c219bbafcbb417e336759baaa043515ead0b
Sungbae <9uiopa@gmail.com>
2023-12-31 19:23:37 +0900

    delete source ~/.bashrc

--- scripts/start.sh
+++ scripts/start.sh
@@ -25,9 +25,6 @@ echo "> $JAR_NAME 실행"
 
 IDLE_PROFILE=$(find_idle_profile)
 
-# shellcheck disable=SC1090
-source ~/.bashrc
-
 echo "> $JAR_NAME 를 profile=$IDLE_PROFILE 로 실행합니다."
 nohup java -jar \
     -Dspring.config.location=classpath:/application.properties,classpath:/application-$IDLE_PROFILE.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
